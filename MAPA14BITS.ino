d7154c1fe618c9a71e8af5aa5ea373a94a884165
DarioJorgeIglesias <49843154+DarioJorgeIglesias@users.noreply.github.com>
2020-03-04 03:34:31 -0300

    Add files via upload

---
+++ MAPA14BITS.ino
@@ -0,0 +1,21 @@
+//Autor:Dario Jorge Iglesias
+boolean D,C,B,A,Z1;
+void setup() 
+{
+ pinMode(8,INPUT);//entrada D
+ pinMode(9,INPUT);//entrada C
+ pinMode(10,INPUT);//entrada B
+ pinMode(11,INPUT);//entrada A
+ pinMode(7,OUTPUT);//Pin 7 salida
+}
+void loop() 
+{
+ D=digitalRead(8);
+ C=digitalRead(9);
+ B=digitalRead(10);
+ A=digitalRead(11);
+ Z1=(!D&A)|(C&A);
+ digitalWrite(7,Z1);
+}
+
+
