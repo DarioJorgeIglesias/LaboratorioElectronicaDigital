36e3bff52c0c3fb3cb347d1e3cb7715422898ffc
DarioJorgeIglesias <49843154+DarioJorgeIglesias@users.noreply.github.com>
2020-03-04 04:06:45 -0300

    Add files via upload

---
+++ mascaraANDOR.ino
@@ -0,0 +1,20 @@
+//Autor:Darío Jorge Iglesias
+#include<avr/IO.h>
+void setup() 
+{
+DDRB =0b11111111;//PB7 pin 13 OUT
+PORTB=0B11111111;//Todo pin a 5V
+}
+void loop() 
+{
+PORTB =PORTB & 0x7F;//0b01111111
+delay(2000);
+PORTB =PORTB | 0x80;//0b10000000
+delay(1000);
+}
+
+
+
+
+
+
