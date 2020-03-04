3b4bcbd4f3a4525f22d85ab04de7fb6fb976581a
DarioJorgeIglesias <49843154+DarioJorgeIglesias@users.noreply.github.com>
2020-03-04 05:13:49 -0300

    Add files via upload

---
+++ latch_S_R.ino
@@ -0,0 +1,23 @@
+//Autor:Dario Jorge Iglesias
+boolean Qn,NQn,NOS,NOR;
+void setup(){
+ pinMode(9,INPUT_PULLUP);//NOS
+ pinMode(10,INPUT_PULLUP);//NOR
+ pinMode(13,OUTPUT);//Qn
+ pinMode(12,OUTPUT);//NQn
+ digitalWrite(13,LOW);
+ digitalWrite(12,LOW);}
+void loop(){
+  NOS=digitalRead(9);
+  NOR=digitalRead(10);
+  Qn=digitalRead(13);
+  NQn=digitalRead(12);
+  Qn=!(NOS&NQn);
+  digitalWrite(13,Qn);
+  NQn=!(NOR&Qn);
+  digitalWrite(12,NQn);}
+
+  
+
+  
+  
