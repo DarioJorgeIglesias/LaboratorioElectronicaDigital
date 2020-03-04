8fe3032d2ddc85041ee6a40f80e04c26e15d3438
DarioJorgeIglesias <49843154+DarioJorgeIglesias@users.noreply.github.com>
2020-03-04 03:30:14 -0300

    Add files via upload

---
+++ XORMINITERMINOS.ino
@@ -0,0 +1,11 @@
+boolean A,B,L;
+void setup(){
+ pinMode(2,INPUT);
+ pinMode(3,INPUT);
+ pinMode(4,OUTPUT);}
+void loop(){
+ A=digitalRead(2);
+ B=digitalRead(3);
+ L=(!A&B)|(A&!B);
+ digitalWrite(4,L);}
+ 
