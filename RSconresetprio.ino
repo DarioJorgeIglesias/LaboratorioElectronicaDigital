00608b80a42aa11cf548e209e4984d26955adab6
DarioJorgeIglesias <49843154+DarioJorgeIglesias@users.noreply.github.com>
2020-03-04 05:15:54 -0300

    Add files via upload

---
+++ RSconresetprio.ino
@@ -0,0 +1,15 @@
+//Autor:Dario Jorge Iglesias
+boolean Qn,S,R,Q;
+void setup(){ 
+ pinMode(9,INPUT);//S
+ pinMode(10,INPUT);//R
+ pinMode(13,OUTPUT);//Qn
+ digitalWrite(13,LOW);}
+void loop() {
+  S=digitalRead(9);
+  R=digitalRead(10);
+  Q=digitalRead(13);
+  Qn=!R&(S|Q);
+  digitalWrite(13,Qn);}
+
+
