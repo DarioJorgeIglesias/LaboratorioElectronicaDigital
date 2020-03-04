407bb1b9ee4586c3cf3a51509c560d99b7cd880a
DarioJorgeIglesias <49843154+DarioJorgeIglesias@users.noreply.github.com>
2020-03-04 04:06:00 -0300

    Add files via upload

---
+++ mascaraand.ino
@@ -0,0 +1,19 @@
+//Autor:Darío Jorge Iglesias
+#include<avr/IO.h>//Se puede omitir
+boolean Pb7;
+void setup()
+{
+//PUERTOB SALIDA PIN13 PB7
+DDRB = 0b11111111;//Todo Out
+PORTB=0B11111111;//Todo pin a 5V
+Serial.begin(9600);//baud=9600
+}
+void loop() 
+{
+Pb7 =(PORTB & 0x80)/128;//0b10000000
+delay(1000);
+Serial.println(Pb7);}
+
+
+
+
