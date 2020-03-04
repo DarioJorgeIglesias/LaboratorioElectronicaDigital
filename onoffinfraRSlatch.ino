ed3ef21b0d17baf6f760a35377fad201e6f36585
DarioJorgeIglesias <49843154+DarioJorgeIglesias@users.noreply.github.com>
2020-03-04 05:20:14 -0300

    Add files via upload

---
+++ onoffinfraRSlatch.ino
@@ -0,0 +1,24 @@
+boolean R,S,Qn,NOQn;
+void setup() 
+{
+pinMode(13,OUTPUT);
+pinMode(6,INPUT);
+pinMode(7,INPUT);
+}
+void loop() 
+{
+R=digitalRead(7);//leemos sensor A
+S=digitalRead(6);//leemos sensor B
+//sensor normalmente entrega HIGH
+R=!R;//negamos asegurando LOW
+S=!S;//negamos asegurando LOW
+//Salidas simple y complementada
+Qn=!(R|NOQn);
+NOQn=!(S|Qn);
+digitalWrite(13,NOQn);}//aseguramos LOW
+
+
+
+
+
+
