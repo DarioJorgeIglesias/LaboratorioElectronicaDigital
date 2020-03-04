3aeb5217153f8355fe837ef5ccb7570fc2703b64
DarioJorgeIglesias <49843154+DarioJorgeIglesias@users.noreply.github.com>
2020-03-04 04:10:16 -0300

    Add files via upload

---
+++ corrimientoderecha.ino.ino
@@ -0,0 +1,15 @@
+//Autor:Darío Jorge Iglesias
+#include<avr/IO.h>
+int I;
+void setup(){ 
+DDRK =0b11111111;//PUERTOK SALIDA
+PORTK=128;}
+void loop(){ 
+for(I=7;I>=0;I--){
+  if(I==7)PORTK=128;
+  else
+  PORTK=PORTK / 2;//División /
+  delay(1000); } }//end loop
+
+           
+           
