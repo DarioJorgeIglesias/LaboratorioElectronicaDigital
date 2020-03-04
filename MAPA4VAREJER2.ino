c4def2861d70c8985bbe10ab9766d3f3e6725648
DarioJorgeIglesias <49843154+DarioJorgeIglesias@users.noreply.github.com>
2020-03-04 03:36:25 -0300

    Add files via upload

---
+++ MAPA4VAREJER2.ino
@@ -0,0 +1,19 @@
+//Autor:Dario Jorge Iglesias
+boolean A,B,C,D,Z;
+void setup() 
+{
+ pinMode(7,INPUT);//A
+ pinMode(8,INPUT);//B
+ pinMode(9,INPUT);//C
+ pinMode(10,INPUT);//D
+ pinMode(13,OUTPUT);//Z
+}
+void loop() {
+ A=digitalRead(7);
+ B=digitalRead(8);
+ C=digitalRead(9);
+ D=digitalRead(10);
+ Z=(!D&!C)|(!B&!A);
+ digitalWrite(13,Z);}
+
+ 
