fe787742c8c6cf72c45aa78bf1e67a8a91cf3142
DarioJorgeIglesias <49843154+DarioJorgeIglesias@users.noreply.github.com>
2020-03-04 02:56:17 -0300

    Add files via upload

---
+++ and2entradas.ino
@@ -0,0 +1,20 @@
+//Autor:Dario Jorge 
+//Iglesias
+boolean A,B,Z;
+void setup() 
+{
+ pinMode(2,INPUT);
+ pinMode(3,INPUT);
+ pinMode(4,OUTPUT);
+}
+void loop() 
+{
+ A=digitalRead(2);
+ B=digitalRead(3);
+ Z=(A&B);//Z=A.B
+ digitalWrite(4,Z);
+}
+
+
+
+
