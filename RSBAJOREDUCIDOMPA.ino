61d91d43cd712b9fec4cf672f2a1fd3e195c881d
DarioJorgeIglesias <49843154+DarioJorgeIglesias@users.noreply.github.com>
2020-03-04 05:15:01 -0300

    Add files via upload

---
+++ RSBAJOREDUCIDOMPA.ino
@@ -0,0 +1,14 @@
+//Autor:Dario Jorge Iglesias
+boolean Qn,S,R,Q;
+void setup(){ 
+ pinMode(9,INPUT_PULLUP);//S
+ pinMode(10,INPUT_PULLUP);//R
+ pinMode(13,OUTPUT);//Qn
+ digitalWrite(13,LOW);}
+void loop() {
+  S=digitalRead(9);
+  R=digitalRead(10);
+  Q=digitalRead(13);
+  Qn=!S|(R&Q);
+  digitalWrite(13,Qn);}
+  
