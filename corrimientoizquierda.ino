386114ef138e6517039f1badab56f1cb4e6f55f9
DarioJorgeIglesias <49843154+DarioJorgeIglesias@users.noreply.github.com>
2020-03-04 04:09:34 -0300

    Add files via upload

---
+++ corrimientoizquierda.ino
@@ -0,0 +1,18 @@
+//Autor:Darío Jorge Iglesias
+#include<avr/IO.h>
+int I;
+void setup(){
+DDRK=0b11111111;//PUERTOK SALIDA
+PORTK=1;}
+void loop(){ 
+for(I=0;I<=7;I++){
+  if(I==0)PORTK=1;
+  else
+  PORTK=PORTK * 2;//Multiplicación *
+  delay(1000);} 
+}//end loop         
+
+
+
+
+           
