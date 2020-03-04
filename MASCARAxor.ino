e7846ce5a7e8bc1b1c84b66b92d11167f173014e
DarioJorgeIglesias <49843154+DarioJorgeIglesias@users.noreply.github.com>
2020-03-04 04:05:14 -0300

    Add files via upload

---
+++ MASCARAxor.ino
@@ -0,0 +1,13 @@
+//Autor:Darío Jorge Iglesias
+#include<avr/IO.h>
+void setup()
+{
+//PUERTOB SALIDA PIN13 PB7
+DDRB =0b11111111;
+PORTB=0B11111111;
+}
+void loop(){
+PORTB=PORTB ^ 0x80;//0b10000000
+delay(1000);}
+
+
