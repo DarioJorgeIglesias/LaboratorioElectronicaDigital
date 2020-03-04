4e3739c9190cf21f2aa8e42cda91ca8a2be744de
DarioJorgeIglesias <49843154+DarioJorgeIglesias@users.noreply.github.com>
2020-03-04 05:25:50 -0300

    Add files via upload

---
+++ voltimetro.ino
@@ -0,0 +1,22 @@
+//Autor:Darío Jorge Iglesias
+float voltaje;
+int lectura;//var. ADC
+boolean A;
+void setup(){
+Serial.begin(9600); 
+pinMode(13,OUTPUT);
+pinMode(12,INPUT);}
+void loop(){
+lectura=analogRead(A0);
+A=digitalRead(12);
+voltaje=lectura * (5.0/1023.0);
+Serial.print(voltaje);
+Serial.print("Salida:");
+Serial.println(A);
+digitalWrite(13,A);
+delay(1000);}
+
+
+
+
+
