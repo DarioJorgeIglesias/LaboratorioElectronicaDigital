22a809a22f8133253b70a4d8af9ac7f027348e8e
DarioJorgeIglesias <49843154+DarioJorgeIglesias@users.noreply.github.com>
2020-03-04 03:00:32 -0300

    Add files via upload

---
+++ nand2entradas.ino
@@ -0,0 +1,18 @@
+//Autor:Dario Jorge 
+//Iglesias
+boolean A,B,Z;
+void setup() 
+{
+ pinMode(9,INPUT);
+ pinMode(10,INPUT);
+ pinMode(13,OUTPUT);
+}
+void loop() 
+{
+ A=digitalRead(9);
+ B=digitalRead(10);
+ Z=!(A&B);//Z='(A.B)
+ digitalWrite(13,Z);
+}
+
+
