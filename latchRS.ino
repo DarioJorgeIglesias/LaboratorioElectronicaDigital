a5aae492953413f89499d6d4e00b81ddfc928347
DarioJorgeIglesias <49843154+DarioJorgeIglesias@users.noreply.github.com>
2020-03-04 05:11:32 -0300

    Add files via upload

---
+++ latchRS.ino
@@ -0,0 +1,20 @@
+//Autor:Dario Jorge Iglesias
+boolean Qn,NQn,S,R;
+void setup() {
+ pinMode(9,INPUT);
+ pinMode(10,INPUT);//S Y R
+ pinMode(13,OUTPUT);//Qn
+ pinMode(12,OUTPUT);//NQn
+ digitalWrite(13,LOW);
+ digitalWrite(12,LOW);}
+void loop() {
+  S=digitalRead(9);
+  R=digitalRead(10);
+  Qn=digitalRead(13);
+  NQn=digitalRead(12);
+  Qn=!(R|NQn);NQn=!(S|Qn);
+  digitalWrite(13,Qn);
+  digitalWrite(12,NQn);}
+  
+
+
