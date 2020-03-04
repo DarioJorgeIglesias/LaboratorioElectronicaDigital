521602a2b959ba2d6147b9c680adb59acdc8ec32
DarioJorgeIglesias <49843154+DarioJorgeIglesias@users.noreply.github.com>
2020-03-04 04:11:29 -0300

    Add files via upload

---
+++ desplazamientoderechaa.ino
@@ -0,0 +1,14 @@
+//Autor:Darío Jorge Iglesias
+#include<avr/IO.h>
+int I;
+void setup(){
+DDRK=0b11111111;//PUERTOK SALIDA
+PORTK=128;}
+void loop(){ 
+for(I=0;I<=7;I++){
+delay(1000);
+PORTK=PORTK>>1; 
+if(PORTK==0)
+PORTK=128;}}
+
+           
