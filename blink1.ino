cfc8928132f6ff44a2bad3683d5d3953a38c25e9
DarioJorgeIglesias <49843154+DarioJorgeIglesias@users.noreply.github.com>
2020-03-04 02:42:20 -0300

    Add files via upload

---
+++ blink1.ino
@@ -0,0 +1,17 @@
+//Autor:Dario Jorge Iglesias
+void setup()//Configuración de pines
+{
+  pinMode(13, OUTPUT);//Pin 13 salida
+}
+void loop()//Ciclo infinito
+{//Inicio loop
+  digitalWrite(13,HIGH);//Saca "1"=5V en pin 13
+  delay(1000);          //Espera 1 Segundo
+  digitalWrite(13,LOW); //Saca "0"=0V en pin 13
+  delay(1000);          //Espera 1 Segundo
+}//Fin loop
+
+
+
+
+
