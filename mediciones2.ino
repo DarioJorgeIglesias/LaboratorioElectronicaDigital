1b72710dcf6e23145e32a07391996113a0296fb5
DarioJorgeIglesias <49843154+DarioJorgeIglesias@users.noreply.github.com>
2020-03-04 05:24:48 -0300

    Add files via upload

---
+++ mediciones2.ino
@@ -0,0 +1,21 @@
+//Autor:Darío Jorge Iglesias
+int lectura;//var. ADC
+boolean A;
+void setup(){
+Serial.begin(9600); 
+pinMode(13,OUTPUT);
+pinMode(12,INPUT);}
+void loop(){
+lectura=analogRead(A0);
+A=digitalRead(12);
+Serial.print(lectura);
+Serial.print("Salida:");
+Serial.println(A);
+digitalWrite(13,A);
+delay(1000);}
+
+
+
+
+
+
