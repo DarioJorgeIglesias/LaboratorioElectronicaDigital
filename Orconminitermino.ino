0ff904db4527ea7dbd7e8936da6a64e68a5ec2c1
DarioJorgeIglesias <49843154+DarioJorgeIglesias@users.noreply.github.com>
2020-03-04 03:28:56 -0300

    Add files via upload

---
+++ Orconminitermino.ino
@@ -0,0 +1,16 @@
+//Autor:Dario Jorge Iglesias
+boolean A,B,m;
+void setup() 
+{
+ pinMode(2,INPUT);
+ pinMode(3,INPUT);
+ pinMode(4,OUTPUT);
+}
+void loop() 
+{
+ A=digitalRead(2);
+ B=digitalRead(3);
+ m=(!A&B)|(A&!B)|(A&B);
+ digitalWrite(4,m);
+}
+
