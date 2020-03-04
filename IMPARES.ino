10f6ccdb9d82d1ef458be02b862515e6e3507c25
DarioJorgeIglesias <49843154+DarioJorgeIglesias@users.noreply.github.com>
2020-03-04 03:47:46 -0300

    Add files via upload

---
+++ IMPARES.ino
@@ -0,0 +1,25 @@
+//Autor:Dario Jorge Iglesias
+//Los números impares NO se 
+//pueden dividir exactamente en 
+//grupos de dos. 
+boolean D,C,B,A,Impares;
+void setup() {
+ pinMode(9,INPUT);//entrada D  
+ pinMode(10,INPUT);//entrada C
+ pinMode(11,INPUT);//entrada B
+ pinMode(12,INPUT);//entrada A
+ pinMode(8,OUTPUT);}//salida pares
+void loop() {
+ D=digitalRead(9); 
+ C=digitalRead(10);
+ B=digitalRead(11); 
+ A=digitalRead(12);
+ Impares=A; 
+ digitalWrite(8,Impares);}
+
+
+ 
+
+
+ 
+
