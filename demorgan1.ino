9ef0feabc7581195d265a9fc6d16d2dc83b7b13d
DarioJorgeIglesias <49843154+DarioJorgeIglesias@users.noreply.github.com>
2020-03-04 03:13:08 -0300

    Add files via upload

---
+++ demorgan1.ino
@@ -0,0 +1,22 @@
+//Autor:Dario Jorge Iglesias
+boolean B,A,Z1,Z2;
+void setup() 
+{
+ pinMode(0,INPUT_PULLUP);//entrada A
+ pinMode(1,INPUT_PULLUP);//entrada B
+ pinMode(8,OUTPUT);//salida Z1='(A.B)
+ pinMode(9,OUTPUT);//salida Z2='A+'B
+}
+void loop() 
+{
+ B=digitalRead(1);//Guarda 1er dato
+ A=digitalRead(0);//Guarda 2do dato
+ Z1=!(A&B);//multiplica data e inv.
+ Z2=!A|!B;//inv. cada dato y sum.
+ digitalWrite(8,Z1);//actualiza
+ digitalWrite(9,Z2);//salidas
+}
+
+
+
+
