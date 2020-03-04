c5bb6bfc62c6e3d270023faf5bc0a4d3f3abeace
DarioJorgeIglesias <49843154+DarioJorgeIglesias@users.noreply.github.com>
2020-03-04 02:43:26 -0300

    Add files via upload

---
+++ blink1.2.ino
@@ -0,0 +1,16 @@
+//Autor:Dario Jorge Iglesias
+void setup()//Conf. de pines
+{
+pinMode(3, OUTPUT);//Pin3 OUT
+}
+void loop()//Ciclo infinito
+{
+  digitalWrite(3, HIGH);//5V 
+  delay(25);//Espera 25mSeg.
+  digitalWrite(3, LOW);//0V
+  delay(25);//Espera 25mSeg.
+}
+
+
+
+
