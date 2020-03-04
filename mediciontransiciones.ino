fa95fd1e921988775aec74c5c890f5b7f22be495
DarioJorgeIglesias <49843154+DarioJorgeIglesias@users.noreply.github.com>
2020-03-04 05:23:47 -0300

    Add files via upload

---
+++ mediciontransiciones.ino
@@ -0,0 +1,15 @@
+//Autor:Darío Jorge Iglesias
+int lectura;//var. ADC
+boolean A;
+void setup(){
+Serial.begin(9600); 
+pinMode(13,OUTPUT);
+pinMode(12,INPUT);}
+void loop(){
+lectura=analogRead(A0);
+Serial.println(lectura);
+A=digitalRead(12);
+digitalWrite(13,A);
+delay(1000);}
+
+
