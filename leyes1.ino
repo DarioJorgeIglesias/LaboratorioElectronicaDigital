ae79b1c25433a1f6715a4670d6307cce0b45e6d5
DarioJorgeIglesias <49843154+DarioJorgeIglesias@users.noreply.github.com>
2020-03-04 03:15:37 -0300

    Add files via upload

---
+++ leyes1.ino
@@ -0,0 +1,29 @@
+//Autor:Dario Jorge Iglesias
+boolean B,A,Z1,Z2;
+void setup(){ 
+ pinMode(2,INPUT_PULLUP);//entrada B
+ pinMode(3,INPUT_PULLUP);//entrada A
+ pinMode(8,OUTPUT);//salida Z1 PARA A
+ pinMode(9,OUTPUT);}//Z2 PARA A+A.B           
+void loop(){
+ B=digitalRead(2);//Variables de In
+ A=digitalRead(3);
+ Z1=A;//FUNCIÓN Z1
+ Z2=A|(A&B);//FUNCIÓN Z2
+ digitalWrite(8,Z1);//refresco de Out
+ digitalWrite(9,Z2);}
+
+
+ 
+           
+
+           
+
+
+
+           
+
+           
+
+
+
