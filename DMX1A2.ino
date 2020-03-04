5370d5cb2d53f3fdf8e7a7872f3cdbacb33df4fa
DarioJorgeIglesias <49843154+DarioJorgeIglesias@users.noreply.github.com>
2020-03-04 04:35:49 -0300

    Add files via upload

---
+++ DMX1A2.ino
@@ -0,0 +1,24 @@
+//Autor:Dario Jorge Iglesias
+boolean A,D0,D1,I;
+void setup() 
+{
+  pinMode(6,INPUT);//Entrada I del DMX
+  pinMode(7,INPUT);//Var. de control A
+  pinMode(8,OUTPUT);//Salida DO
+  pinMode(9,OUTPUT);//Salida DI
+}
+void loop() 
+{
+  I=digitalRead(6);//Leemos canal 
+  A=digitalRead(7);//variable de control
+  D0=!A&I;//Funcion Salida DO
+  D1=A&I;//Funcion Salida DI
+  digitalWrite(8,D0);//Saca dato I por DO
+  digitalWrite(9,D1);//Saca dato I por DI
+}
+
+
+
+
+
+
