6da7101df962bcf5715ee729a48e023cae1f3ba0
DarioJorgeIglesias <49843154+DarioJorgeIglesias@users.noreply.github.com>
2020-03-04 03:23:32 -0300

    Add files via upload

---
+++ MINITERMINOCOMPLETO.ino
@@ -0,0 +1,17 @@
+//Autor:Dario Jorge Iglesias //MINITÉRMINOS
+boolean C,B,A,Z;//Variables de entrada y salida
+void setup() //Configuración
+{
+ pinMode(9,INPUT);//C
+ pinMode(10,INPUT);//B
+ pinMode(11,INPUT);//A
+ pinMode(6,OUTPUT);//Pin 6 salida Z
+}
+void loop()        { //Super lazo infinito
+ C=digitalRead(9); //Lee peso 4
+ B=digitalRead(10);//Lee peso 2
+ A=digitalRead(11);//Lee peso 1
+ Z=(!C&!B&A)|(!C&B&A)|(C&!B&!A)|(C&!B&A);
+ digitalWrite(6,Z);}//Refresca salida Z
+           
+
