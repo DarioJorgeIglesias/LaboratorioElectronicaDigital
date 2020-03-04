b6f1c7758a1b5f5be2118198018d1058def2758f
DarioJorgeIglesias <49843154+DarioJorgeIglesias@users.noreply.github.com>
2020-03-04 03:31:04 -0300

    Add files via upload

---
+++ AndconMaxitermino.ino
@@ -0,0 +1,17 @@
+//Autor:Dario Jorge Iglesias
+boolean A,B,M;
+void setup() 
+{
+ pinMode(2,INPUT);
+ pinMode(3,INPUT);
+ pinMode(4,OUTPUT);
+}
+void loop() 
+{
+ A=digitalRead(2);
+ B=digitalRead(3);
+ M=(A|B)&(A|!B)&(!A|B);
+ digitalWrite(4,M);
+}
+
+
