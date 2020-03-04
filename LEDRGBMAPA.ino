985898f079a29e369990e8adcfb578f9150696e1
DarioJorgeIglesias <49843154+DarioJorgeIglesias@users.noreply.github.com>
2020-03-04 03:43:03 -0300

    Add files via upload

---
+++ LEDRGBMAPA.ino
@@ -0,0 +1,30 @@
+//Autor:Dario Jorge Iglesias
+boolean A,B,C,D,LR,LB,LG;
+//LG = LED GREEN (VERDE)
+//LB = LED BLUE (AZUL)
+//LR = LED REED (ROJO)
+void setup() 
+{
+ pinMode(8,INPUT);//entrada D
+ pinMode(9,INPUT);//entrada C
+ pinMode(10,INPUT);//entrada B
+ pinMode(11,INPUT);//entrada A
+ pinMode(6,OUTPUT);//salida LB
+ pinMode(5,OUTPUT);//salida LG
+ pinMode(4,OUTPUT);//salida LR
+}
+void loop() 
+{
+ D=digitalRead(8);
+ C=digitalRead(9);
+ B=digitalRead(10);
+ A=digitalRead(11);
+ LB=(D&C)|(D&B);
+ digitalWrite(6,LB);
+ LG=(!D&C&A)|(!D&C&B)|(D&!C&!B);
+ digitalWrite(5,LG);
+ LR=(!D&!C)|(!D&!B&!A);
+ digitalWrite(4,LR);
+}   
+
+
