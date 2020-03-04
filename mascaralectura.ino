302ca2a1ef229e02b9a17f2cb396e22f0cbef880
DarioJorgeIglesias <49843154+DarioJorgeIglesias@users.noreply.github.com>
2020-03-04 04:07:31 -0300

    Add files via upload

---
+++ mascaralectura.ino
@@ -0,0 +1,16 @@
+//Autor:Darío Jorge Iglesias
+#include<avr/IO.h>
+boolean Pb6;
+void setup()
+{
+//PUERTOB ENTRADA PIN12 Pb6
+DDRB = 0b10111111;
+Serial.begin(9600);
+}
+void loop(){
+Pb6 =boolean((PINB & 0x40)/64);//0b01000000
+delay(1000);
+Serial.println(Pb6);}
+
+
+
