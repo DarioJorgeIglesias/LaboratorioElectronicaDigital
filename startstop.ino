2b47aee8c357b1ac401abbdc25294fdd805d6ac2
DarioJorgeIglesias <49843154+DarioJorgeIglesias@users.noreply.github.com>
2020-03-04 05:10:37 -0300

    Add files via upload

---
+++ startstop.ino
@@ -0,0 +1,19 @@
+//Autor:Dario Jorge Iglesias
+boolean START,STOP,Z,Z1;
+void setup()
+{ 
+ pinMode(2,INPUT);
+ pinMode(3,INPUT);
+ pinMode(4,OUTPUT);
+}
+void loop()
+{ 
+ START=digitalRead(2);
+ STOP=digitalRead(3);
+ Z=Z1&!STOP;
+ Z1=START|(Z1&!STOP);
+ digitalWrite(4,Z1);
+ }
+
+
+
