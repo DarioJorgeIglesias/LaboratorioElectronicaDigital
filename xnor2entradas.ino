63f14ba9bb949698addbf296dd1750ddc166d476
DarioJorgeIglesias <49843154+DarioJorgeIglesias@users.noreply.github.com>
2020-03-04 03:09:30 -0300

    Add files via upload

---
+++ xnor2entradas.ino
@@ -0,0 +1,21 @@
+//Autor:Dario Jorge 
+//Iglesias
+boolean A,B,Z;
+void setup() 
+{
+ pinMode(9,INPUT);
+ pinMode(10,INPUT);
+ pinMode(13,OUTPUT);
+}
+void loop() 
+{
+ A=digitalRead(9);
+ B=digitalRead(10);
+ Z=!(A^B);//Z='(A XOR B)
+ digitalWrite(13,Z);
+}
+
+
+
+
+
